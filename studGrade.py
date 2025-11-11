d19e970b2d58f623e11e413441fbfb1aa5c440ff
Whritix <01fe24bca043@kletech.ac.in>
2025-11-11 08:43:49 +0530

    jenkins3

--- studGrade.py
+++ studGrade.py
@@ -19,6 +19,14 @@ else:
     mark5 = "35"
     print("No input given, using default values:")
 
+total = int(mark1) + int(mark2) + int(mark3) + int(mark4) + int(mark5)
+average = total / 5 
 print(" Name:", name)
 print("Student Name:", name)
-print("Roll Number:", rollno)
+print("Subject 1 marks :", mark1)
+print("Subject 2 marks :", mark2)
+print("Subject 3 marks :", mark3)
+print("Subject 4 marks :", mark4)
+print("Subject 5 marks :", mark5)
+print("Total Marks:", total)
+print("Average Marks:", average)
