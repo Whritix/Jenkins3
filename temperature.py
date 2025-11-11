5205c2cd17f0a8fc8318ce2c243308f0d783fa27
Whritix <01fe24bca043@kletech.ac.in>
2025-11-11 10:07:53 +0530

    meow

--- temperature.py
+++ temperature.py
@@ -1,17 +1,14 @@
 import sys
-if len(sys.argv) == 2:
+
+if len(sys.argv) >= 2:
     script_name = sys.argv[0]
-    temp = sys.argv[1]
-    
+    temp = float(sys.argv[1])
 else:
-    temp = '25'
-    
-if temp<15:
-    print("Cold Weather")
-    
-elif temp<=30:
-    print("Normal Weather")
-else:   
-    print("Hot Weather")
-    
-        
+    temp = 25.0
+
+if temp < 15:
+    print("cold")
+elif temp <= 30:
+    print("normal")
+else:
+    print("hot")
